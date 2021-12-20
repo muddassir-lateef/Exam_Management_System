69bbe11c9efacde1f932053b6fb4161ae45151bc
ShayanZuberi <i190749@nu.edu.pk>
2021-12-20 21:48:23 +0500

    Submit feedback, Register Exam commit

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,6 +1,18 @@
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Student extends User{
 
+    @OneToMany
+    private List<Exam> exams;
+
+    public void addExam(Exam e){
+        exams.add(e);
+    }
+
+    public List<Exam> getExams() {
+        return exams;
+    }
 }
--- src/main/java/SubmitFeedbackController.java
+++ src/main/java/SubmitFeedbackController.java
@@ -1,13 +1,22 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class SubmitFeedbackController {
 
     @FXML
-    private ComboBox<?> examsDropdown;
+    private ComboBox<String> examsDropdown;
 
     @FXML
     private Button selectExamBtn;
@@ -17,6 +26,28 @@ public class SubmitFeedbackController {
 
     @FXML
     private Button submitBtn;
+
+    public void initialize() {
+        ObservableList<String> options1 = FXCollections.observableArrayList();
+        Configuration con = new Configuration();
+        con.configure().addAnnotatedClass(Exam.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction trans = session.beginTransaction();
+
+        int invID = 6;  //set this from singleton variable
+        List exams = session.createQuery("FROM Exam where assignedInvigilator.ID = :invID").list();
+        for (Iterator iter = exams.iterator(); iter.hasNext(); ) {
+
+            Exam exm = (Exam) iter.next();
+            options1.add(exm.getId() + ". " + exm.getName());
+
+        }
+        trans.commit();
+
+        examsDropdown.setItems(options1);
+
+    }
 
     @FXML
     void selectExamClicked(ActionEvent event) {
