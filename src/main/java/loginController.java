231390e14db6d2b7e1bfc52f70f23d29fdc47678
Muddassir <lateef.muddassir@gmail.com>
2021-12-20 12:22:31 +0500

    Login implemented

--- src/main/java/loginController.java
+++ src/main/java/loginController.java
@@ -7,8 +7,14 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 public class loginController {
 
@@ -29,6 +35,25 @@ public class loginController {
 
     @FXML
     void LoginBtnClicked(ActionEvent event) {
+        Configuration con = new Configuration();
+        con.configure().addAnnotatedClass(LoginDetails.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session  = sf.openSession();
+        Transaction trans = session.beginTransaction();
+
+        LoginDetails usrs = (LoginDetails) session.createQuery("FROM LoginDetails U WHERE U.username = :userName AND U.password= :password").setParameter("userName", usrnameField.getText()).setParameter("password",pswdField.getText()).uniqueResult();
+
+        if(usrs!=null)
+        {
+            promptField.setText("Login Successful!");
+
+        }
+        else
+        {
+            promptField.setText("Login Failed!");
+
+        }
+        trans.commit();
 
     }
 
