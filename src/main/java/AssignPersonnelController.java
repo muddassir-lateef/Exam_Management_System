20d033ddfe8ec4ce4e50be4183aa9c6a264158bd
ShayanZuberi <i190749@nu.edu.pk>
2021-12-20 17:18:15 +0500

    Staff Screens Commit

---
+++ src/main/java/AssignPersonnelController.java
@@ -0,0 +1,37 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
+
+public class AssignPersonnelController {
+
+    @FXML
+    private ComboBox<?> ExamsComboBox;
+
+    @FXML
+    private Text addVenueLabel;
+
+    @FXML
+    private Button assignBtn;
+
+    @FXML
+    private ComboBox<?> personnelComboBox;
+
+    @FXML
+    private Button selectBtn;
+
+    @FXML
+    private ComboBox<?> typeComboBox;
+
+    @FXML
+    void assignBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectBtnClicked(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/RegistrationCodesController.java
@@ -0,0 +1,9 @@
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class RegistrationCodesController {
+
+    @FXML
+    private Text regCodesLabel;
+
+}
