4d7d02d69b34c284aba57a334df720ed2b61a2a4
ShayanZuberi <i190749@nu.edu.pk>
2021-12-20 22:43:49 +0500

    View Exam Feedback

--- src/main/java/ExamFeedbackController.java
+++ src/main/java/ExamFeedbackController.java
@@ -1,3 +1,5 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -5,6 +7,13 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class ExamFeedbackController {
 
@@ -18,7 +27,7 @@ public class ExamFeedbackController {
     private Button examSelectBtn;
 
     @FXML
-    private ComboBox<?> examsComboBox;
+    private ComboBox<String> examsComboBox;
 
     @FXML
     private TextArea feedbackField;
@@ -26,13 +35,66 @@ public class ExamFeedbackController {
     @FXML
     private TextField invigilatorField;
 
+    public void initialize() {
+        ObservableList<String> options1 = FXCollections.observableArrayList();
+        Configuration con = new Configuration();
+        con.configure().addAnnotatedClass(Invigilator.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction trans = session.beginTransaction();
+
+        List exams = session.createQuery("FROM Exam").list();
+        for (Iterator iter = exams.iterator(); iter.hasNext(); ) {
+
+            Exam exm = (Exam) iter.next();
+            options1.add(exm.getId() + ". " + exm.getName());
+
+        }
+        trans.commit();
+
+        examsComboBox.setItems(options1);
+
+    }
+
     @FXML
     void examSelectBtnClicked(ActionEvent event) {
+        String[] temp1 = examsComboBox.getValue().split(". ");
+        int eid=Integer.parseInt(temp1[0]);
+
+        ObservableList<String> options1 = FXCollections.observableArrayList();
+        Configuration con = new Configuration();
+        con.configure().addAnnotatedClass(Invigilator.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction trans = session.beginTransaction();
+
+        Exam exam = (Exam)session.createQuery("FROM Exam where Id = :temp").setParameter("temp", eid).uniqueResult();
+
+        invigilatorField.setText(exam.getAssignedInvigilator().getName());
+
 
     }
 
     @FXML
     void viewBtnClicked(ActionEvent event) {
+        ObservableList<String> options1 = FXCollections.observableArrayList();
+        Configuration con = new Configuration();
+        con.configure().addAnnotatedClass(Invigilator.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction trans = session.beginTransaction();
+
+        String[] temp1 = examsComboBox.getValue().split(". ");
+        int eid=Integer.parseInt(temp1[0]);
+
+        Exam exam = (Exam)session.createQuery("FROM Exam where Id = :temp").setParameter("temp", eid).uniqueResult();
+
+        Feedback fb= (Feedback) session.createQuery("FROM Feedback where exam.Id = :temp").setParameter("temp", exam.getId()).uniqueResult();
+
+        feedbackField.setText(fb.getFeedbackStatement());
+
+
+
 
     }
 
